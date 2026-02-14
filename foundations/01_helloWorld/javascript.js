c45c5520d1e396a6c42a1148e3d1b91f3a3637a2
EmptyBusSeats <keanehoxie@gmail.com>
2026-02-14 21:34:08 +0000

    Update foundations

---
+++ foundations/01_helloWorld/javascript.js
@@ -0,0 +1,21 @@
+function add7(number) {
+    return number + 7;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function capitalize(string2Return) {
+    return newString = string2Return.charAt(0).toUpperCase() + string2Return.substring(1).toLowerCase();
+}
+
+function lastLetter(char2Return) {
+    return char2Return[char2Return.length - 1];
+}
+
+console.log('add7 = ' + add7(10));
+console.log('multiply =' + multiply(3,2));
+console.log('capitalize = ' + capitalize('ABCDEFghIJK'));
+console.log('lastLetter =' + lastLetter('jkjkgjkj2kjksjkjgkjkgjkajhkahifohakeuhkhqz123'));
+
